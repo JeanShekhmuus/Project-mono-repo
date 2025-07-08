8641f509fcedd0884d2b34328d9627bd0a9cdcd4
JeanShekhmuus <33418894+JeanShekhmuus@users.noreply.github.com>
2025-07-08 17:40:51 +0200

    Update app.module.ts
    Password removed

--- MicroWarehouse/micro-warehouse-backend/src/app.module.ts
+++ MicroWarehouse/micro-warehouse-backend/src/app.module.ts
@@ -6,7 +6,7 @@ import { BuilderModule } from './modules/builder/builder.module';
 import { HttpModule } from '@nestjs/axios';
 
 @Module({
-    imports: [MongooseModule.forRoot('mongodb+srv://JeanShekhmuus:Jean-1234-@jeancluster.a19aw.mongodb.net/JeanDb?retryWrites=true&w=majority'), BuilderModule, HttpModule],
+    imports: [MongooseModule.forRoot('"MongoDB-Creds"'), BuilderModule, HttpModule],
   controllers: [AppController],
   providers: [AppService],
 })
